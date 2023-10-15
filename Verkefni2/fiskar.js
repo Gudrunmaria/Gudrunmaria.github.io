d64bd0fafd01bde6a6aaee5457ba31b1f1fff1f1
Gudrunmaria <80835961+Gudrunmaria@users.noreply.github.com>
2023-10-15 23:29:29 +0000

    Add files via upload

--- Verkefni2/fiskar.js
+++ Verkefni2/fiskar.js
@@ -6,6 +6,14 @@ var NumBody = 6;
 var NumTail = 3;
 var Numfinn = 3;
 
+//kassinn sem fiskarnir eiga að vera í 
+var kassiMinX = -0.8;
+var kassiMaX = 0.8;
+var kassiMinY = -0.8;
+var kassiMaY = 0.8;
+var kassiMinZ = -0.8;
+var kassiMaZ = 0.8;
+
 // Hn�tar fisks � xy-planinu
 var vertices = [
     // l�kami (spjald)
@@ -21,16 +29,44 @@ var vertices = [
     vec4( -0.65, -0.15, 0.0, 1.0 ),
 
     //Uggi 1
-    vec4(0.0, 0.05,0.0,1.0),
+    vec4(0.0,0.05,0.1,1.0),
     vec4(0.1,0.1,0.0,1.0),
     vec4(0.1,-0.05,0.0,1.0),
-
+    
     //Uggi 2
-    vec4(0.0, 0.05,0.0,1.0),
-    vec4(0.1,-0.1,0.0,1.0),
+    vec4(0.0,0.05,-0.1,1.0),
+    vec4(0.1,0.1,0.0,1.0),
     vec4(0.1,-0.05,0.0,1.0)
 ];
 
+//litir á fiskunum
+var litir =[
+    vec4(0.2, 0.6, 0.9, 1.0),
+    vec4(0.3, 0.3, 0.6, 1.0),
+    vec4(0.3,0.7,0.7,1.0),
+    vec4(0.2,0.5,0.7,1.0),
+    vec4(0.5,0.4,0.7,1.0),
+    vec4(0.5,0.87,0.57,1.0),
+    vec4(0.17,0.8,0.58,1.0),
+    vec4(0.9,0.4,0.57,1.0),
+    vec4(0.75,0.54,0.82,1.0),
+    vec4(0.54,0.54,0.83,1.0),
+    vec4(0.12,0.9,0.5,1.0)
+];
+
+//upphafleg staðsetning fiskana
+var upphaf = [
+    0.0, 0.6, 0.3,
+    0.4, 0.7, 0.2,
+    -0.6, 0.3, -0.2,
+    -0.7, -0.3, -0.4,
+    0.6, 0.1, -0.6,
+    -0.3, -0.6, 0.6,
+    0.4, -0.5, 0.5,
+    -0.2, -0.2, -0.2,
+    0.5, -0.6, -0.6,
+    0.2, 0.5, -0.6
+];
 
 var movement = false;     // Er m�sarhnappur ni�ri?
 var spinX = 0;
@@ -39,9 +75,12 @@ var origX;
 var origY;
 
 var rotTail = 0.0;        // Sn�ningshorn spor�s
-var incTail = 2.0;        // Breyting � sn�ningshorni
+var incTail = 0.5;        // Breyting � sn�ningshorni
 
-var zView = 2.0;          // Sta�setning �horfanda � z-hniti
+var rotFinn =0.0;          
+var incFinn =0.3;
+
+var zView = 4.0;          // Sta�setning �horfanda � z-hniti
 
 var proLoc;
 var mvLoc;
@@ -129,6 +168,53 @@ window.onload = function init()
     render();
 }
 
+function fiskur(mv, x,y,z,litur){
+    gl.uniform4fv(colorLoc,litur);
+
+    rotTail += incTail;
+    if( rotTail > 35.0  || rotTail < -35.0 )
+        incTail *= -1;
+
+
+    rotFinn += incFinn;
+    if(rotFinn >30.0 || rotFinn < -35.0)
+        incFinn *=-1;
+        
+
+    //Teikna búk
+    mv = mult(mv,translate(x,y,z));
+
+    var mv1 =mv;
+
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays( gl.TRIANGLES, 0, NumBody );
+
+    //Teikna sporð og snúa honum
+    mv = mult( mv, translate ( -0.5, 0.0, 0.0 ) );
+    mv = mult( mv, rotateY( rotTail ) );
+	mv = mult( mv, translate ( 0.5, 0.0, 0.0 ) );
+	
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays( gl.TRIANGLES, NumBody, NumTail );
+
+    gl.uniform4fv(colorLoc,vec4(0.0,0.2,0.3,1.0));
+    
+    //Teikna ugga1
+    mv =mult(mv1, translate(0.0,0.0,0.0));
+    mv =mult(mv, rotateY(rotFinn));
+  
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays(gl.TRIANGLES, NumBody+NumTail,Numfinn);
+
+    //Teikna ugga2
+    mv =mult(mv1, translate(0.0,0.0,0.0));
+    mv =mult(mv, rotateY(rotFinn));
+
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays(gl.TRIANGLES, NumBody+NumTail+Numfinn,Numfinn);
+
+}
+
 
 function render()
 {
@@ -138,29 +224,13 @@ function render()
     mv = mult( mv, rotateX(spinX) );
     mv = mult( mv, rotateY(spinY) );
 
-    rotTail += incTail;
-    if( rotTail > 35.0  || rotTail < -35.0 )
-        incTail *= -1;
-
-	gl.uniform4fv( colorLoc, vec4(0.2, 0.6, 0.9, 1.0) );
-
-	// Teikna l�kama fisks (�n sn�nings)
-    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
-    gl.drawArrays( gl.TRIANGLES, 0, NumBody );
-
-    // Teikna spor� og sn�a honum
-	mv = mult( mv, translate ( -0.5, 0.0, 0.0 ) );
-    mv = mult( mv, rotateY( rotTail ) );
-	mv = mult( mv, translate ( 0.5, 0.0, 0.0 ) );
-	
-    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
-    gl.drawArrays( gl.TRIANGLES, NumBody, NumTail );
-
-    mv = mult(mv, translate(0.0,0.0,0.0));
-
-    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
-    gl.drawArrays(gl.TRIANGLES, NumBody+NumTail, Numfinn);
-
+    for(let i =0; i <10;i++) {
+        const startIndex =i*3;
+       
+        fiskur(mv,upphaf[startIndex],upphaf[1+startIndex],upphaf[2+startIndex],litir[i]);
+        
+    }
+    
     requestAnimFrame( render );
 }
 
